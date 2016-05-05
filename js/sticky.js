61af429c77fef47bf0d14ccfd2709606dc2614ee
hyan <h.defreitas@umiami.edu>
2016-05-05 13:19:25 -0400

    minimal style changes

--- js/sticky.js
+++ js/sticky.js
@@ -1,17 +1,12 @@
 $(document).ready(function() {
-  //change the integers below to match the height of your upper div, which I called
-  //banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
-  //to figure out what the scroll position is when exactly you want to fix the nav
-  //bar or div or whatever.  I stuck in the console.log for you.  Just remove when
-  //you know the position.
+  
   var headerHeight = $("header.hero").outerHeight();
   var $nav = $("nav.nav");
  
 
   
   $(window).scroll(function () {
-    console.log("scrollTop: " + $(window).scrollTop());
-    console.log("headerHeight: " + headerHeight);
+    
 
     if ($(window).scrollTop() > headerHeight) {
       $("body").addClass('nav-fixed-top');
@@ -23,3 +18,5 @@ $(document).ready(function() {
   });
 });
 
+
+// adapted from: http://codepen.io/jparkerweb/pen/ConKw //
